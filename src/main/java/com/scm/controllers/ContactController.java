e465e60f73bccf56d15780d932c96e0427e2942b
Sanya Singh <ssanyaa@amazon.com>
2026-01-24 22:13:06 +0530

    Validation on Contact Form

--- src/main/java/com/scm/controllers/ContactController.java
+++ src/main/java/com/scm/controllers/ContactController.java
@@ -1,26 +1,28 @@
 package com.scm.controllers;
+
 import com.scm.entities.Contact;
 import com.scm.entities.User;
 import com.scm.forms.ContactForm;
 import com.scm.helpers.Helper;
+import com.scm.helpers.Message;
+import com.scm.helpers.MessageType;
 import com.scm.services.ContactService;
 import com.scm.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.ui.Model;
-
-
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.util.Optional;
-
 @Controller
 @RequestMapping("/user/contacts")
 public class ContactController {
-    
+
     @Autowired
     ContactService contactService;
 
@@ -31,26 +33,34 @@ public class ContactController {
     @RequestMapping("/add")
     public String addContactView(Model model) {
         ContactForm contactForm = new ContactForm();
-        contactForm.setName("Anurag Tiwari");
         model.addAttribute("contactForm", contactForm);
         return "user/add_contact";
     }
 
     // we're sending contact data to server using POST method (ref- add_contact form field method=post).
     // So this saveContact function need to be POST
-    @RequestMapping(value="/add", method = RequestMethod.POST)
-    public String saveContact(@ModelAttribute ContactForm contactForm, Authentication authentication) {
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public String saveContact(@Valid @ModelAttribute ContactForm contactForm, BindingResult result,
+                              Authentication authentication, HttpSession session) {
         System.out.println(contactForm);
-        
+
         /* process the form data */
 
-        // TODO: validate the form
+        // validate the form
+        if (result.hasErrors()) {
+            session.setAttribute("message",
+                    Message.builder()
+                            .content("Please correct the following errors")
+                            .type(MessageType.red)
+                            .build());
+            return "user/add_contact";
+        }
 
         // convert the ContactForm to Contact entity because we have received ContactForm & we've to save Contact
         // form -> Contact
         // processing User of the Contact
-         String username = Helper.getEmailOfLoggedInUser(authentication);
-         User user = userService.getUserByEmail(username).orElse(null);
+        String username = Helper.getEmailOfLoggedInUser(authentication);
+        User user = userService.getUserByEmail(username).orElse(null);
 
         // TODO: process the contact profile pic
         Contact contact = new Contact();
@@ -68,7 +78,12 @@ public class ContactController {
         // save the Contact
         contactService.save(contact);
         System.out.println("Saving Contact......");
-        // TODO: set message to be displayed on the view
+        // set message to be displayed on the view
+        session.setAttribute("message",
+                Message.builder()
+                        .content("You have successfully added a new contact")
+                        .type(MessageType.green)
+                        .build());
         return "redirect:/user/profile";
     }
 }
--- src/main/java/com/scm/forms/ContactForm.java
+++ src/main/java/com/scm/forms/ContactForm.java
@@ -1,5 +1,8 @@
 package com.scm.forms;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +17,25 @@ import org.springframework.web.multipart.MultipartFile;
 @ToString
 public class ContactForm {
 
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @Email(message = "Invalid Email Address")
     private String email;
+
+    @NotBlank(message = "Phone Number is required")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Invalid Phone Number")
     private String phoneNumber;
+
+    @NotBlank(message = "Address is required")
     private String address;
+
     private String description;
+
     private boolean favorite;
+
     private String websiteLink;
+
     private String linkedinLink;
 
     private MultipartFile profileImage;
